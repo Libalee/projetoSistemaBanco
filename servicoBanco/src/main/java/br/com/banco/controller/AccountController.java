5f179078f58cc165a5846d973f73ed7584c967da
Libalee <joaovictor.alvino@gmail.com>
2024-01-12 16:03:07 -0300

    Hateoas and pageable fully added and operational

--- servicoBanco/src/main/java/br/com/banco/controller/AccountController.java
+++ servicoBanco/src/main/java/br/com/banco/controller/AccountController.java
@@ -1,6 +1,15 @@
 package br.com.banco.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.converter.MathConverter;
@@ -24,20 +34,27 @@ public class AccountController {
 	@Autowired
 	AccountService accountService;
 	
-//	@GetMapping
-//	public ResponseEntity<?> findAll(
-//			@RequestParam(value="page", defaultValue = "0") int page,
-//			@RequestParam(value="limit", defaultValue = "10") int limit,
-//			@RequestParam(value="direction", defaultValue = "asc") String direction
-//			) {
-//		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
-//		
-//		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection));
-//		
-//		Page<AccountVO> accounts = accountService.findAll(pageable);
-//		
-//		return new ResponseEntity<>(accounts, HttpStatus.OK);
-//	}
+	@GetMapping
+	public Page<AccountVO> findAll(
+			@RequestParam(value="page", defaultValue = "0") int page,
+			@RequestParam(value="limit", defaultValue = "10") int limit,
+			@RequestParam(value="direction", defaultValue = "asc") String direction
+			) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "accountNumber"));
+		
+		
+		Page<AccountVO> accounts = accountService.findAll(pageable);
+		
+		accounts.stream().forEach(accountVO -> accountVO.add(
+				linkTo(methodOn(AccountController.class)
+						.findById(Long.toString(accountVO.getKey()))).withSelfRel()));
+		
+		
+		
+		return accounts;
+	}
 	
 	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
 	public AccountVO findById(@PathVariable("id") String id) {
