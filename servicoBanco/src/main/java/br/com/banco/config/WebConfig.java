87e4ad12be0852c4ad5d44c03ead9a5a7d12f9b1
Libalee <joaovictor.alvino@gmail.com>
2024-01-12 09:20:59 -0300

    Added support for Yaml and XML data formats

--- servicoBanco/src/main/java/br/com/banco/config/WebConfig.java
+++ servicoBanco/src/main/java/br/com/banco/config/WebConfig.java
@@ -1,10 +1,15 @@
 package br.com.banco.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.banco.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
@@ -22,6 +27,11 @@ public class WebConfig implements WebMvcConfigurer{
 			.defaultContentType(MediaType.APPLICATION_JSON)
 			.mediaType("json", MediaType.APPLICATION_JSON)
 			.mediaType("xml", MediaType.APPLICATION_XML)
-			.mediaType("yaml", MediaType.valueOf("aplication/x-yaml"));
+			.mediaType("yaml", MediaType.valueOf("application/x-yaml"));
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
 	}
 }
